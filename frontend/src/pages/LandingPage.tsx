e2f97e45927fe3029a43cf3a3a64b5544072e489
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-24 11:32:40 -0300

    feat(frontend): use cards in the main page and improve styles - Use three cards with different titles and descriptions explaining the page. - Create a global style for the buttons containers. - Improve cards spacing in the titles and subtitles.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,3 +1,5 @@
+import Card from "../shared/components/Cards/Card";
+
 export default function LandingPage() {
     return (
         <section>
@@ -12,6 +14,23 @@ export default function LandingPage() {
                     Login
                 </button>
             </div>
+
+            <section className="buttons-container">
+                <Card
+                    title="Real-time practice"
+                    subtitle="Generate random numbers considering the difficult and mode"
+                />
+
+                <Card
+                    title="Track your progress"
+                    subtitle="Help you through your practices tracking improvements"
+                />
+
+                <Card
+                    title="Multiple levels and modes"
+                    subtitle="Choose your difficult level and the type of operation you want to practice"
+                />
+            </section>
         </section>
     );
 };
