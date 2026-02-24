0d86b8caf0f228fa6eb5a5f304442d4331bba107
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-24 11:15:55 -0300

    feat(frontend): add basic structure and styles for the Card component

---
+++ frontend/src/shared/components/Cards/Card.tsx
@@ -0,0 +1,14 @@
+import type { cardProps } from "../../types/cardProps";
+import Style from './Card.module.css';
+
+function Card({ title, subtitle}: cardProps) {
+    return (
+        <article className={Style.card}>
+            <h3 className={`heading-tertiary ${Style.cardTitle}`}>{title}</h3>
+
+            <p className={Style.cardSubtitle}>{subtitle}</p>
+        </article>
+    )
+};
+
+export default Card;
