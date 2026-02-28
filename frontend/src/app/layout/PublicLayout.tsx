87c63fdb6060a46283e4b0a6fe8c0eee31ffd2e4
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-28 13:39:29 -0300

    feat(frontend): add basic structure to the public layout

--- frontend/src/app/layout/PublicLayout.tsx
+++ frontend/src/app/layout/PublicLayout.tsx
@@ -1,9 +1,18 @@
+import { Outlet } from "react-router";
+import { Footer } from "../../shared/components/Semantic/Semantic";
+
 // This is the public wrapper. This layout will be used to wrap the landing page. Its purpose is to avoid non registered users go to pages they cannot use such as the dashboard.
 
 function PublicLayout() {
     return (
         <>
-            
+            <main
+                id="main-content"
+                tabIndex={-1}
+            >
+                <Outlet />
+            </main>
+            <Footer />
         </>
     )
 };
