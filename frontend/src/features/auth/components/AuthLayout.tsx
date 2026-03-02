670e829db9393349b982d5a6f1c74ef7fc196c4b
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-02 15:59:08 -0300

    feat(frontend): fix AuthLayout width with new CSS utilities

--- frontend/src/features/auth/components/AuthLayout.tsx
+++ frontend/src/features/auth/components/AuthLayout.tsx
@@ -2,7 +2,7 @@ import type { AuthLayoutProps } from "../../../shared/types/AuthProps";
 
 function AuthLayout({ children, id }: AuthLayoutProps) {
     return (
-        <section id={id} className="transparent-border">
+        <section id={id} className="transparent-border flex flex-column justify-center m-auto max-w-700px">
             {children}
         </section>
     )
