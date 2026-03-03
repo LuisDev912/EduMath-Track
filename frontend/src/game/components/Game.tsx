89c428a3a7a4c81872b912c2435a4e71fdbd97b6
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-03 11:03:10 -0300

    feat(frontend): create initial game configuration

--- frontend/src/game/components/Game.tsx
+++ frontend/src/game/components/Game.tsx
@@ -1,5 +1,30 @@
+import { Activity, useState } from "react";
+
 function Game() {
-    
+    const [gameConfig, setGameConfig] = useState({
+        firstNumber: 0,
+        secondNumber: 0,
+        isCorrect: false,
+        error: false
+    });
+
+    const generateNumbers = () => {
+        const a = Math.floor(Math.random() * 50) + 1;
+        const b = Math.floor(Math.random() * 50) + 1;
+
+
+    }
+
+    return (
+        <section className="box">
+
+            <Activity mode="hidden">
+                <p>
+                    example
+                </p>
+            </Activity>
+        </section>
+    )
 };
 
 export default Game;
