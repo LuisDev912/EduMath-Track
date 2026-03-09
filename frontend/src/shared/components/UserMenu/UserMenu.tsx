e0100cf556562845457f0bde028826fc6ac1ca59
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-09 17:09:27 -0300

    style(frontend): add basic styles to the user icon

--- frontend/src/shared/components/UserMenu/UserMenu.tsx
+++ frontend/src/shared/components/UserMenu/UserMenu.tsx
@@ -1,13 +1,9 @@
-import { useState, Activity } from 'react';
 import Avatar from './Avatar/Avatar';
 import Styles from './UserMenu.module.css';
 
 function UserMenu() {
-    const [open, setOpen] = useState(false);
-
     return (
-        <div className='user-menu'>
-
+        <div className={Styles.profileIcon}>
             <Avatar name="John Doe" />
         </div>
     );
