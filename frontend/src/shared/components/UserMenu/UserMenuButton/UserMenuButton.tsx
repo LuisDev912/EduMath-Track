307385a7c89985ba6301afb8c23680066054aecc
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-11 18:46:24 -0300

    feat(frontend): create a basic HTML structure for the user menu button

--- frontend/src/shared/components/UserMenu/UserMenuButton/UserMenuButton.tsx
+++ frontend/src/shared/components/UserMenu/UserMenuButton/UserMenuButton.tsx
@@ -1,5 +1,17 @@
-function UserMenuButton() {
+import Avatar from "../Avatar/Avatar";
+import type { UserMenuButtonProps } from "./UserMenuButtonProps";
+
+function UserMenuButton({ isOpen, onToggle, userName }: UserMenuButtonProps) {
     
+    return (
+        <button
+            type="button"
+            className="userMenuButton"
+            onClick={onToggle}
+        >
+            <Avatar />
+        </button>
+    )
 };
 
 export default UserMenuButton;
