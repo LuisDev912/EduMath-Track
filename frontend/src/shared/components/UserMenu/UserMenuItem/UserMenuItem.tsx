bb3df0451961cbac0bc226751af6c7de321a3091
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-12 19:34:11 -0300

    feat(frontend): improve navigation in UserMenuItem

--- frontend/src/shared/components/UserMenu/UserMenuItem/UserMenuItem.tsx
+++ frontend/src/shared/components/UserMenu/UserMenuItem/UserMenuItem.tsx
@@ -1,7 +1,23 @@
 import type { UserMenuItemProps } from './UserMenuItemProps';
+import { NavLink } from 'react-router';
 import Styles from '../UserMenu.module.css';
 
-function UserMenuItem({ children, onClick }: UserMenuItemProps) {
+function UserMenuItem({ children, onClick, to }: UserMenuItemProps) {
+    if (to) {
+        return (
+            <li role="none">
+                <NavLink
+                    to={to}
+                    className={Styles.itemLink}
+                    role="menuitem"
+                    onClick={onClick}
+                >
+                    {children}
+                </NavLink>
+            </li>
+        );
+    };
+
     return (
         <li role="none">
             <button
--- frontend/src/shared/components/UserMenu/UserMenuItem/UserMenuItemProps.ts
+++ frontend/src/shared/components/UserMenu/UserMenuItem/UserMenuItemProps.ts
@@ -3,4 +3,5 @@ import type { ReactNode } from 'react';
 export type UserMenuItemProps = {
     children: ReactNode;
     onClick?: () => void;
+    to?: string;
 };
