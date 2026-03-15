cda49686f541e2b72ca41f6a9ea29a18b504c92f
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-15 09:39:58 -0300

    feat(frontend): add a type for the UserMenu component

--- frontend/src/shared/components/UserMenu/UserMenu.tsx
+++ frontend/src/shared/components/UserMenu/UserMenu.tsx
@@ -1,12 +1,18 @@
 import Avatar from './Avatar/Avatar';
 import Styles from './UserMenu.module.css';
 
-function UserMenu() {
+export type UserMenuProps = {
+    userName?: string;
+};
+
+function UserMenu({ userName }: UserMenuProps) {
+    
+
     return (
         <div className={Styles.profileIcon}>
             <Avatar name="John Doe" />
         </div>
     );
-};
+}
 
 export default UserMenu;
