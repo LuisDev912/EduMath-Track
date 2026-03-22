493394f39b4d466fce99db01541f22b34dfd498e
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-22 11:36:01 -0300

    feat(utilities): create a new CSS utilitie for a full width

--- frontend/src/game/components/GenerateOperation.tsx
+++ frontend/src/game/components/GenerateOperation.tsx
@@ -3,7 +3,13 @@ import { useTranslation } from "react-i18next";
 function GenerateOperation({ onGenerate }: { onGenerate: () => void }) {
     const { t } = useTranslation();
 
-    return <button type="button" onClick={onGenerate}>{t("game.generate")}</button>
+    return <button
+        className="full-width"
+        type="button"
+        onClick={onGenerate}
+    >
+        {t("game.generate")}
+    </button>
 };
 
 export default GenerateOperation;
