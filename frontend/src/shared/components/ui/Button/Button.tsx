3442ca3100b68ccc36de683384368a63a8ad1ee5
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-23 11:04:45 -0300

    feat(UI): configure Button component with its props

--- frontend/src/shared/components/ui/Button/Button.tsx
+++ frontend/src/shared/components/ui/Button/Button.tsx
@@ -1,6 +1,26 @@
-import ButtonStyles from './Button.module.scss';
+import ButtonStyles from './Button.module.css';
+import type { ButtonProps } from './Button.types';
 
 // this Button component is for improve consistency across the app, and to make it easier to change the styles of all buttons in one place
-export function Button() {
-    
+export function Button({
+    variant = "primary",
+    size = "md",
+    fullWidth = false,
+    children,
+    ...props
+}: ButtonProps) {
+    return (
+        <button
+            className={[
+                ButtonStyles.button,
+                ButtonStyles[variant],
+                // e.g. ButtonStyles.sm, ButtonStyles.md, ButtonStyles.lg
+                ButtonStyles[size],
+                fullWidth ? ButtonStyles.fullWidth : ''
+            ].join(' ')}
+            {...props}
+        >
+            {children}
+        </button>
+    );
 };
