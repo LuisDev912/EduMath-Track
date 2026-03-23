fcfbfbfe1be938bb82a9f8e60e1636aaede94fb0
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-23 10:22:11 -0300

    feat(types): create a file to contain the Button props

--- frontend/src/shared/components/ui/Button/Button.tsx
+++ frontend/src/shared/components/ui/Button/Button.tsx
@@ -1,5 +1,6 @@
 import ButtonStyles from './Button.module.scss';
 
+// this Button component is for improve consistency across the app, and to make it easier to change the styles of all buttons in one place
 export function Button() {
     
 };
---
+++ frontend/src/shared/components/ui/Button/Button.types.ts
@@ -0,0 +1,11 @@
+export type ButtonProps = {
+    children: React.ReactNode;
+    variant?: "primary" | "secondary" | "ghost" | "danger";
+    size?: "sm" | "md" | "lg";
+    fullWidth?: boolean
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+// primary: main action (blue)
+// secondary: alternative
+// ghost: no background
+// danger: logout/delete
