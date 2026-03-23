1ef593e3edf30926d0948ebe8141ee03b29d2cf0
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-23 12:44:16 -0300

    feat(Input): add basic HTML structure for Input component

--- frontend/src/shared/components/ui/Input/Input.tsx
+++ frontend/src/shared/components/ui/Input/Input.tsx
@@ -1,10 +1,19 @@
-import InputStyles from './Input.module.css';
 import type { InputProps } from './Input.types';
+import InputStyles from './Input.module.css';
 
 // this Input component is for improve consistency across the app, and to make it easier to change the styles of all inputs in one place
-export function Input({
-    label,
-    error,
-    ...props
-}: InputProps) { 
+
+export function Input({ label, error, ...props }: InputProps) {
+    return (
+        <div className={InputStyles.wrapper}>
+            {label && <label>{label}</label>}
+
+            <input
+                className={InputStyles.input}
+                {...props}
+            />
+
+            {error && <span className={InputStyles.error}>{error}</span>}
+        </div>
+    )
 };
