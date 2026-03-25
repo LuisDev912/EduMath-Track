5f3ab2f47f3c87968e00dee8cec488761d556785
Luis Calleja <luiscallejaenrique@gmail.com>
2026-03-25 11:59:27 -0300

    feat(structure): improve SettingsLayout HTML structure and accessibility

--- frontend/src/features/settings/pages/SettingsLayout.tsx
+++ frontend/src/features/settings/pages/SettingsLayout.tsx
@@ -1,3 +1,40 @@
+import { Outlet, useLocation } from "react-router";
+import SettingsHeader from "../components/SettingsHeader";
+import { useRef, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
 export default function SettingsLayout() {
-    return <main></main>
+    const location = useLocation();
+    const { t } = useTranslation();
+    const mainRef = useRef<HTMLElement>(null);
+    
+    useEffect(() => {
+        if (mainRef.current) mainRef.current.focus();
+    }, [location.pathname]);
+    return (
+        <>
+            <SettingsHeader />
+
+            <button
+                className="skip-link"
+                onClick={() => {
+                    mainRef.current?.focus();
+                }}
+            >
+                {t("layout.skipToContent")}
+            </button>
+
+            <main
+                ref={mainRef}
+                tabIndex={-1}
+                className="settings-layout"
+            >
+
+                <section className="settings-content">
+                    <Outlet />
+                </section>
+
+            </main>
+        </>
+    );
 };
