836eac933a144bc49b02f685bd67d2ab14c1fd10
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-05 18:37:08 -0300

    feat(components): create files for a new NavBar component

---
+++ frontend/src/shared/components/ui/NavBar/NavBar.tsx
@@ -0,0 +1,10 @@
+import { NavProps } from './Nav.types.ts';
+import Styles from './NavBar.module.css';
+
+export function NavBar() {
+    return (
+        <nav className={Styles.navBar}>
+            
+        </nav>
+    );
+};
