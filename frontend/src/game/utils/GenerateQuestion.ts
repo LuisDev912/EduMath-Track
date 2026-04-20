787f9e07f0a2e55e79db66d71c0c801a9b06bea3
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-20 15:30:01 -0300

    feat(GenerateQuestion): add a switch case for each mode

--- frontend/src/game/utils/GenerateQuestion.ts
+++ frontend/src/game/utils/GenerateQuestion.ts
@@ -16,4 +16,38 @@ export function generateQuestion(
     let firstNumber = Math.floor(Math.random() * max) + 1
     let secondNumber = Math.floor(Math.random() * max) + 1
 
+    switch (mode) {
+        case "addition":
+            return {
+                operator: "+",
+                answer: firstNumber + secondNumber
+            };
+
+        case "subtraction":
+            if (firstNumber < secondNumber) { // ensure the first number is greater than the second to avoid negative answers
+                [firstNumber, secondNumber] = [secondNumber, firstNumber]
+            };
+            
+            return {
+                operator: "-",
+                answer: firstNumber - secondNumber
+            };
+
+        case "multiplication":
+            return {
+                operator: "X",
+                answer: firstNumber * secondNumber
+            };
+
+        case "division":
+            while (secondNumber === 0 || firstNumber % secondNumber !== 0) { // ensure the second number is not zero and the first number is divisible by the second to avoid fractional answers
+                firstNumber = Math.floor(Math.random() * max) + 1
+                secondNumber = Math.floor(Math.random() * max) + 1
+            };
+
+            return {
+                operator: "÷",
+                answer: firstNumber / secondNumber
+            };
+    };
 }
