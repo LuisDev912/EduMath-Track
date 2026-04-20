f3dba7b71a787f4bdc3b7c8c07614b143cee43dc
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-20 15:25:29 -0300

    Revert "feat(useGame): define the number ranges for each difficulty"
    This reverts commit f3696f7b490b0da2ef91e0a9a06a4ab59e334682.

--- frontend/src/game/hooks/useGame.tsx
+++ frontend/src/game/hooks/useGame.tsx
@@ -1,20 +1,3 @@
-import type { Difficulty, GameMode, Question } from "../types/Game.types";
-
-export function generateQuestion(
-    mode: GameMode,
-    difficulty: Difficulty
-): Question {
-    const ranges = {
-        tutorial: 5,
-        easy: 10,
-        medium: 50,
-        hard: 100
-    }
-
-    const max = ranges[difficulty]
-
-    let firstNumber = Math.floor(Math.random() * max) + 1
-    let secondNumber = Math.floor(Math.random() * max) + 1
-
+export function useGame() {
     
-}
+};
