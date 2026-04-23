a5aeb77c46af166f95e6a57643e3462dde3b5228
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-23 16:40:38 -0300

    feat(game): import types and create states for the useGame hook

--- frontend/src/game/hooks/useGame.tsx
+++ frontend/src/game/hooks/useGame.tsx
@@ -1,3 +1,21 @@
-export function useGame() {
-    
+import { useState } from "react";
+import type { Difficulty, GameMode } from "../types/Game.types";
+import { generateQuestion } from "../utils/GenerateQuestion";
+
+export function useGame(
+    mode: GameMode,
+    difficulty: Difficulty
+) {
+    // State variables
+    const [score, setScore] = useState(0);
+    const [question, setQuestion] = useState(() => {
+        generateQuestion(mode, difficulty);
+    });
+
+    // Functions
+
+    return {
+        score,
+        question
+    };
 };
