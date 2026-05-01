561fc6630d47c272f0cb7153b0c3d7af52ae3ef4
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-01 18:48:29 -0300

    fix(a11y): fix heading level in Card component - Change the heading level from `h3` to `p` in the Card component to improve accessibility, as the card title is not a section heading and should not be marked up as such.

--- frontend/src/shared/components/Cards/Card.tsx
+++ frontend/src/shared/components/Cards/Card.tsx
@@ -4,7 +4,7 @@ import Style from './Card.module.css';
 function Card({ title, subtitle }: cardProps) {
     return (
         <article className={Style.card}>
-            <h3 className={`heading-tertiary ${Style.cardTitle}`}>{title}</h3>
+            <p className={`heading-tertiary ${Style.cardTitle}`}>{title}</p>
 
             <p className={Style.cardSubtitle}>{subtitle}</p>
         </article>
