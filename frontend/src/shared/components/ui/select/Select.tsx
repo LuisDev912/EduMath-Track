481e03d972b4f8032afd87fa634fce7fb1c5013b
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-02 11:55:42 -0300

    feat(Select): improve types for the Select component props

--- frontend/src/shared/components/ui/select/Select.tsx
+++ frontend/src/shared/components/ui/select/Select.tsx
@@ -2,7 +2,13 @@ import { useId } from 'react';
 import type { SelectProps } from './Select.types';
 import Styles from './Select.module.css';
 
-export function Select({ id }: SelectProps) {
+export function Select({
+    id,
+    label,
+    value,
+    options,
+    action,
+}: SelectProps) {
     const selectId = useId();
     return (
         <select id={id || selectId} className={Styles.select}></select>
--- frontend/src/shared/components/ui/select/Select.types.ts
+++ frontend/src/shared/components/ui/select/Select.types.ts
@@ -1,3 +1,7 @@
 export type SelectProps = {
+    label: string;
+    value: string;
+    options: string[];
+    action: (value?: string) => void;
     id?: string;
-}
+};
