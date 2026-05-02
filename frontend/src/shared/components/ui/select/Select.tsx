01b8424534dc99314aac220cbea5f686b67d47eb
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-02 11:52:13 -0300

    feat(Select): create basic HTML structure for the Select component

--- frontend/src/shared/components/ui/select/Select.tsx
+++ frontend/src/shared/components/ui/select/Select.tsx
@@ -1,5 +1,10 @@
+import { useId } from 'react';
+import type { SelectProps } from './Select.types';
 import Styles from './Select.module.css';
 
-export function Select() {
-
-}
+export function Select({ id }: SelectProps) {
+    const selectId = useId();
+    return (
+        <select id={id || selectId} className={Styles.select}></select>
+    );
+};
