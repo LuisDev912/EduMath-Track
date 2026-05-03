a91355d96b1c81c65e53a5b5c2cba8e1c0a4624d
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-03 13:08:46 -0300

    feat(game): improve FinalLink component types - Add basic types for FinalLink component such as the mode and difficulty props. - If no mode or difficulty is provided, the component will default to "addition" mode and "easy" difficulty.

--- frontend/src/game/hub/components/FinalLink/FinalLink.tsx
+++ frontend/src/game/hub/components/FinalLink/FinalLink.tsx
@@ -1,5 +1,14 @@
-function FinalLink() {
-    
+import type { FinalLinkProps } from "./FinalLink.types.ts";
+
+function FinalLink({
+    mode,
+    difficulty
+}: FinalLinkProps) {
+    // if the mode or difficulty is not provided, we will use the default values
+    const finalMode = mode || "addition";
+    const finalDifficulty = difficulty || "easy";
+
+    return <a href="#">test</a>
 };
 
 export default FinalLink;
---
+++ frontend/src/game/hub/components/FinalLink/FinalLink.types.ts
@@ -0,0 +1,4 @@
+export type FinalLinkProps = {
+    mode: string;
+    difficulty: string;
+};
