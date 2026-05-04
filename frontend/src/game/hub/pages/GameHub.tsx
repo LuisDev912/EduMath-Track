d407d0c7103f2df80ecd14e652676716008f8908
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-04 15:49:50 -0300

    feat(game): use a test of FinalLink component in GameHub page

--- frontend/src/game/hub/pages/GameHub.tsx
+++ frontend/src/game/hub/pages/GameHub.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
 import Styles from '../../gameplay/components/Game.module.css';
 
+import FinalLink from "../components/FinalLink/FinalLink";
+
 // this is the hub page for the game. Here, the user will select the wanted game mode and difficulty. The user will also be able to see their stats and achievements.
 
 export default function GameHub() {
@@ -11,7 +13,14 @@ export default function GameHub() {
             <div className={Styles.box}>
                 <h1>{t("game.hub.title")}</h1>
                 <p>{t("game.hub.description")}</p>
+                
+                {/* hardcoded for testing purposes */}
+                <FinalLink
+                    mode="addition"
+                    difficulty="easy"
+                />
             </div>
+
         </section>
     );
 };
