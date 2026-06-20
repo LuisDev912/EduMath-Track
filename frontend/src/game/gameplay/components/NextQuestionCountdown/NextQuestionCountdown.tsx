d20caa4aefeb28963414f42bde559da8d3766cc8
Luis Calleja <luiscallejaenrique@gmail.com>
2026-06-20 15:51:42 -0300

    feat(game): update NextQuestionCountdown timer to show the seconds left

--- frontend/src/game/gameplay/components/NextQuestionCountdown/NextQuestionCountdown.tsx
+++ frontend/src/game/gameplay/components/NextQuestionCountdown/NextQuestionCountdown.tsx
@@ -1,15 +1,32 @@
-import type { NextQuestionCountdownProps } from "./NextQuestionCountdown.types.ts";
 import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react";
+import type { NextQuestionCountdownProps } from "./NextQuestionCountdown.types.ts";
 
 function NextQuestionCountdown({ countdown }: NextQuestionCountdownProps) {
     const { t } = useTranslation();
     const displayCountdown = Math.ceil(countdown);
+    const [seconds, setSeconds] = useState(displayCountdown);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSeconds((prev) => {
+                if (prev > 0) {
+                    return prev - 1;
+                } else {
+                    clearInterval(timer);
+                    return 0;
+                }
+            });
+
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [displayCountdown]);
 
     return (
         <p aria-live="polite">
             {t("game.nextQuestion",
                 {
-                    countdown: displayCountdown,
+                    countdown: seconds,
                 }
             )}
         </p>
