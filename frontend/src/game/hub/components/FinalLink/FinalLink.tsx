9e05c6f041019392ba0ec4ea1a8334c63aadf167
Luis Calleja <luiscallejaenrique@gmail.com>
2026-07-27 16:48:09 -0300

    refactor(FinalLink): improve navigation and button styling - Use `useNavigate` from `react-router-dom` for navigation instead of directly setting `window.location.href`. - Delete default `variant` prop from the `Button` component to allow for more flexible styling.

--- frontend/src/game/hub/components/FinalLink/FinalLink.tsx
+++ frontend/src/game/hub/components/FinalLink/FinalLink.tsx
@@ -1,5 +1,6 @@
 import type { FinalLinkProps } from "./FinalLink.types.ts";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 import { Button } from '@components/ui/Button/Button.tsx';
 import { ArrowIcon } from '@shared/icons/ArrowIcon.tsx';
@@ -11,18 +12,19 @@ function FinalLink({
     const finalMode = mode || "addition";
     const finalDifficulty = difficulty || "easy";
 
+    const navigate = useNavigate();
     const { t } = useTranslation();
 
     return (
-        <Button
-            variant="primary"
-            size="lg"
-            textDirection="center"
-            onClick={() => {
-                window.location.href = `/game/${finalMode}/${finalDifficulty}`;
-            }}
+            <Button
+                variant="primary"
+                size="sm"
+                onClick={() => navigate(`/game/${finalMode}/${finalDifficulty}`)}
         >
-            {t("game.hub.startButton")}
+            <p className="m-sm text-sm">
+                {t("game.hub.startButton")}
+            </p>
+
             <ArrowIcon />
         </Button>
     );
