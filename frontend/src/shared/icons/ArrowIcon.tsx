10731f1653e9a854c365f9bc9b4d78bd2bf005a5
Luis Calleja <luiscallejaenrique@gmail.com>
2026-07-27 16:18:37 -0300

    feat(icons): create a new arrow icon

---
+++ frontend/src/shared/icons/ArrowIcon.tsx
@@ -0,0 +1,5 @@
+export function ArrowIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" stroke="#fff" viewBox="0 0 24 24"><path id="SVGRepo_iconCarrier" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 12h12m0 0-5-5m5 5-5 5"/></svg>
+    );
+};
