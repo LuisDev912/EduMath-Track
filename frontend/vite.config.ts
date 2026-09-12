fef8594da8efd964a12b3a562324f4e022b1780f
Luis Calleja <luiscallejaenrique@gmail.com>
2026-09-12 16:55:37 -0300

    fix(paths): update alias paths for the game files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     alias: {
       "@components": resolve(__dirname, "src/shared/components"),
       "@shared": resolve(__dirname, "src/shared"),
-      "@game": resolve(__dirname, "src/game")
+      "@game": resolve(__dirname, "src/features/game")
     }
   }
 });
